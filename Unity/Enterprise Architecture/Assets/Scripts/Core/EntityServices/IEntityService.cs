f7514ab8b854df21997350e4e487c707e5cebe86
GenoImel <genoimel@gmail.com>
2023-09-20 16:22:42 -0400

    Updated XML documentation

--- Unity/Enterprise Architecture/Assets/Scripts/Core/EntityServices/IEntityService.cs
+++ Unity/Enterprise Architecture/Assets/Scripts/Core/EntityServices/IEntityService.cs
@@ -1,5 +1,8 @@
 namespace RootName.Core.EntityServices
 {
+    /// <summary>
+    /// For generic typing of specific Entity Services.
+    /// </summary>
     internal interface IEntityService
     {
     }
--- Unity/Enterprise Architecture/Assets/Scripts/Core/Services/IService.cs
+++ Unity/Enterprise Architecture/Assets/Scripts/Core/Services/IService.cs
@@ -1,5 +1,8 @@
 namespace RootName.Core.Services
 {
+    /// <summary>
+    /// For generic typing of specific Services.
+    /// </summary>
     internal interface IService
     {
     }
